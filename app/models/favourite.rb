1d0bf76484a14f7c55709dbc3211d875e8651905
Matthew Fox <mattfoxsydney@gmail.com>
2024-04-05 17:47:06 +1000

    Validations added to models

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,4 +1,8 @@
 class Favourite < ApplicationRecord
   belongs_to :space
   belongs_to :user
+
+  validates :space_id, presence: true
+  validates :user_id, presence: true
+  validates :space_id, uniqueness: { scope: :user_id, message: "has already been added to favourites" }
 end
--- app/models/space.rb
+++ app/models/space.rb
@@ -1,3 +1,8 @@
 class Space < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
