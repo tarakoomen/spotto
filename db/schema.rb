35c16563bb5b44db37fa5f9b1201de2cd80b5029
Tara Koomen <tarakoomen88@gmail.com>
2024-04-11 21:52:21 +1000

    migration for spaces

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_11_084309) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_11_110505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_11_084309) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["user_id"], name: "index_spaces_on_user_id"
   end
 
