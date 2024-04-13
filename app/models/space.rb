40d6c0c4b0a4471d4e6522b2ef5218f7b906993d
Matthew Fox <mattfoxsydney@gmail.com>
2024-04-13 15:25:18 +1000

    Booking Show update, Tara updates

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,7 +1,7 @@
 class Space < ApplicationRecord
   belongs_to :user
   # has_many :favourites
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
   has_one_attached :photo
 
